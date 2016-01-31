c5513baea87e97f67e3722487560450620715726
Julio Gutierrez <bubuntux@gmail.com>
2016-01-31 01:15:47 -0600

    even faster on phones!

--- server/methods.js
+++ server/methods.js
@@ -2,5 +2,8 @@
 Meteor.methods({
 	createPlayer: function (player) {
 		Players.upsert({userId: player.userId}, player);
+	},
+	move: function (artifact) {
+		Artifacts.update({name: artifact.name}, {$set: {x: artifact.x, y: artifact.y}});
 	}
 });
