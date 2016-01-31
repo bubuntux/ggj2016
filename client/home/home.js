a6899775c7c4541bfa88570d8f596697a31bb102
Julio Gutierrez <bubuntux@gmail.com>
2016-01-30 20:48:17 -0600

    use cursor

--- client/home/home.js
+++ client/home/home.js
@@ -1,24 +1,39 @@
 Template.home.onRendered(function () {
 	createjs.Ticker.setFPS(30); //TODO 60 ?
 
-	_.each(this.data, function (player) {
-		let time = player.items.name;
-		let stage = new createjs.Stage(time);
+	let stages = {};
+	stages['present'] = new createjs.Stage('present');
+	stages['past'] = new createjs.Stage('past');
+	stages['future'] = new createjs.Stage('future');
+
+	_.each(_.values(stages), function (stage) {
 		createjs.Touch.enable(stage);
 		createjs.Ticker.addEventListener("tick", stage);
-		let color = 'red'; //TODO remove later or something..
-		if (time === 'present') {
-			color = 'blue';
-		} else if (time === 'future') {
-			color = 'green';
+	});
+
+	this.data.observe({
+		added: function (player) {
+			let time = player.items.name;
+
+			let color = 'red'; //TODO remove later or something..
+			if (time === 'present') {
+				color = 'blue';
+			} else if (time === 'future') {
+				color = 'green';
+			}
+			let items = player.items;
+			_.each(items.arr, function (item) {
+				let shape = new createjs.Shape();
+				shape.name = item.name;
+				shape.x = item.pos.x;
+				shape.y = item.pos.y;
+				shape.graphics.beginFill(color).drawRect(0, 0, 15, 15); //TODO change for images
+				stages[items.name].addChild(shape);
+			});
+		},
+		removed: function (player) {
+			stages[player.items.name].removeAllChildren();
 		}
-		_.each(player.items.arr, function (item) {
-			let shape = new createjs.Shape();
-			shape.x = item.pos.x;
-			shape.y = item.pos.y;
-			shape.graphics.beginFill(color).drawRect(0, 0, 15, 15); //TODO change for images
-			stage.addChild(shape);
-		});
 	});
 
 });
