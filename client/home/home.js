72f6672083c52bcdc9cc838b4a6986e1c41ef285
Julio Gutierrez <bubuntux@gmail.com>
2016-01-30 20:15:42 -0600

    basic split functionallity, (fetch is giving issues)

--- client/home/home.js
+++ client/home/home.js
@@ -1,52 +1,24 @@
-Template.home.helpers({
-	allItems: function () {
-		return _.values(Items);
-	}
-});
-
 Template.home.onRendered(function () {
-
-	/*//TODO only for players?
-	let drop = new createjs.Shape();
-	drop.graphics.beginFill('white').drawRect(400, 600, 50, 50);
-	stage.addChild(drop);
-	//TODO only for players?
-*/
-
 	createjs.Ticker.setFPS(30); //TODO 60 ?
 
-	_.each(_.keys(Items), function (item) {
-		var stage = new createjs.Stage(item);
-
-		let color = 'red'; //TODO remove later or something..
-		if (item === 'present') {
-			color = 'blue';
-		} else if (item === 'future') {
-			color = 'green';
-		}
-
-		_.each(Items[item], function (i) {
-			let shape = new createjs.Shape();
-			shape.x = i.pos.x;
-			shape.y = i.pos.y;
-			shape.graphics.beginFill(color).drawRect(0, 0, 15, 15); //TODO change for images
-			stage.addChild(shape);
-			shape.on("pressmove", function (evt) {
-				evt.target.x = evt.stageX;
-				evt.target.y = evt.stageY;
-			});
-			shape.on("pressup", function (evt) {
-				if (stage.getObjectsUnderPoint(evt.stageX, evt.stageY).length >= 2) {
-					alert('Wow'); //TODO
-				} else {
-					shape.x = i.pos.x;
-					shape.y = i.pos.y;
-				}
-			});
-		});
-
+	_.each(this.data, function (player) {
+		let time = player.items.name;
+		let stage = new createjs.Stage(time);
 		createjs.Touch.enable(stage);
 		createjs.Ticker.addEventListener("tick", stage);
+		let color = 'red'; //TODO remove later or something..
+		if (time === 'present') {
+			color = 'blue';
+		} else if (time === 'future') {
+			color = 'green';
+		}
+		_.each(player.items.arr, function (item) {
+			let shape = new createjs.Shape();
+			shape.x = item.pos.x;
+			shape.y = item.pos.y;
+			shape.graphics.beginFill(color).drawRect(0, 0, 15, 15); //TODO change for images
+			stage.addChild(shape);
+		});
 	});
 
 });
--- client/player/player.js
+++ client/player/player.js
@@ -1,6 +1,55 @@
+/*
 "use strict";
 Template.player.helpers({
 	myItems: function () {
 		return Items[this.items];
 	}
 });
+
+
+Template.home.onRendered(function () {
+
+	/!*!//TODO only for players?
+	 let drop = new createjs.Shape();
+	 drop.graphics.beginFill('white').drawRect(400, 600, 50, 50);
+	 stage.addChild(drop);
+	 //TODO only for players?
+	 *!/
+
+	createjs.Ticker.setFPS(30); //TODO 60 ?
+
+	_.each(_.keys(Items), function (item) {
+		var stage = new createjs.Stage(item);
+
+		let color = 'red'; //TODO remove later or something..
+		if (item === 'present') {
+			color = 'blue';
+		} else if (item === 'future') {
+			color = 'green';
+		}
+
+		_.each(Items[item], function (i) {
+			let shape = new createjs.Shape();
+			shape.x = i.pos.x;
+			shape.y = i.pos.y;
+			shape.graphics.beginFill(color).drawRect(0, 0, 15, 15); //TODO change for images
+			stage.addChild(shape);
+			shape.on("pressmove", function (evt) {
+				evt.target.x = evt.stageX;
+				evt.target.y = evt.stageY;
+			});
+			shape.on("pressup", function (evt) {
+				if (stage.getObjectsUnderPoint(evt.stageX, evt.stageY).length >= 2) {
+					alert('Wow'); //TODO
+				} else {
+					shape.x = i.pos.x;
+					shape.y = i.pos.y;
+				}
+			});
+		});
+
+		createjs.Touch.enable(stage);
+		createjs.Ticker.addEventListener("tick", stage);
+	});
+
+});*/
